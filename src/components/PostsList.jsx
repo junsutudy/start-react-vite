9692a6202ecf68715d0beb6a090cdb232db706e6
junsuPark <krpjs0508@gmail.com>
2024-08-16 17:20:03 +0900

    fallback text

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -18,13 +18,25 @@ function PostsList({ isPosting, onCloseModal }) {
           <NewPost onCancel={onCloseModal} onSubmit={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {[
-          posts.map((element) => (
-            <Post key={Post.body} author={element.author} body={element.body} />
-          )),
-        ]}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {[
+            posts.map((element) => (
+              <Post
+                key={Post.body}
+                author={element.author}
+                body={element.body}
+              />
+            )),
+          ]}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some</p>
+        </div>
+      )}
     </>
   );
 }
