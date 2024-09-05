2bf4a7c8edf1ac71cc51e9632964b1f1ea5f0b81
junsuPark <krpjs0508@gmail.com>
2024-09-05 11:03:23 +0900

    cleanup

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -3,20 +3,10 @@ import MainHeader from "../components/MainHeader";
 import { useState } from "react";
 
 function Posts() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-
-  function showModalHandler() {
-    setModalIsVisible(true);
-  }
-
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
-
   return (
     <>
       <main>
-        <PostsList isPosting={modalIsVisible} onCloseModal={hideModalHandler} />
+        <PostsList />
       </main>
     </>
   );
